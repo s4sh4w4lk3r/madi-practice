18e5f8e903af4f7f3d6a3f8e1cb4223597ebbf40
madiuser@outlook.com <madi@outlook.com>
2023-11-24 11:59:57 +0300

    добавил кнопки и перевел поля в таблице

--- src/components/JsonTable.vue
+++ src/components/JsonTable.vue
@@ -1,23 +1,40 @@
 <template>
     <div>
+        <div>
+            <input class="add-button" type="button" value="Добавить">
+        </div>
+        <br>
+        <div>
+            <input class="delete-button" type="button" value="Удалить">
+        </div>
+        <br>
+        <div>
+            <input class="update-button" type="button" value="Изменить">
+        </div>
+        <br>
+        <br>
+    </div>
+
+    <div>
         <table>
+            <caption>Анкетирование абитуриентов</caption>
             <thead>
                 <tr>
                     <th> Id</th>
-                    <th> Lastname</th>
-                    <th> Firstname</th>
-                    <th> Patronymic</th>
+                    <th> Фамилия</th>
+                    <th> Имя</th>
+                    <th> Отчество</th>
                     <th> Email</th>
-                    <th> Phone</th>
-                    <th> EduState</th>
+                    <th> Номер телефона</th>
+                    <th> Образование</th>
                     <th> GoMadi</th>
-                    <th> Year</th>
-                    <th> Courses</th>
-                    <th> Agree</th>
-                    <th> Subscribe</th>
-                    <th> Created At</th>
-                    <th> Updated At</th>
-                    <th> Deleted At</th>
+                    <th> Год поступления</th>
+                    <th> Курсы</th>
+                    <th> Согласие</th>
+                    <th> Подписка</th>
+                    <th> Создано в </th>
+                    <th> Обновлено в</th>
+                    <th> Удалено в</th>
                 </tr>
             </thead>
             <tbody>
@@ -54,7 +71,38 @@ export default {
         }
     },
     mounted() {
-        this.items = json.root;
+        json.root.forEach(element => {
+            if (element.courses === true) { element.courses = "Да"; }
+            else { element.courses = "Нет"; }
+
+            if (element.agree === true) { element.agree = "Да"; }
+            else { element.agree = "Нет"; }
+
+            if (element.subscribe === true) { element.subscribe = "Да"; }
+            else { element.subscribe = "Нет"; }
+
+            if (element.gomadi === null) { element.gomadi = "Неизв."; }
+
+            if (element.eduState === 0) { element.eduState = "Школа"; }
+            if (element.eduState === 1) { element.eduState = "Колледж"; }
+            if (element.eduState === 2) { element.eduState = "Вуз"; }
+
+            this.items = json.root;
+        });
     }
 }
-</script>`
+</script>
+
+<style>
+.add-button {
+    margin-left: 70%;
+}
+
+.delete-button {
+    margin-left: 70%;
+}
+
+.update-button {
+    margin-left: 70%;
+}
+</style>
