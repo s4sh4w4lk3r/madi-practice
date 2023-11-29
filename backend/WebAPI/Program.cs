0e980ae337a36e726ed9602020ee0cc7f5d9892f
madiuser@outlook.com <madi@outlook.com>
2023-11-29 13:14:17 +0300

    раскидал по папкам

---
+++ backend/WebAPI/Program.cs
@@ -0,0 +1,15 @@
+namespace backend
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
